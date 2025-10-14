f020636fb6b04ae0873b67ee372da741bd708695
Jensaboi <jens.lundin1005@gmail.com>
2025-10-14 23:31:32 +0200

    Bugg fix. Removed file that wasnt used that git was tracking. Replaced with the correct one.

--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import { Outlet } from "react-router-dom";
+
 export default function DefaultLayout() {
   return (
     <div className="min-h-screen">
       <Header />
-      <main>
+      <main className="p-2">
         <Outlet />
       </main>
       <Footer />
