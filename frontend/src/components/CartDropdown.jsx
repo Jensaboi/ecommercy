704eb499e7779ae3c581e39e40539f2330ca1e72
Jensaboi <jens.lundin1005@gmail.com>
2025-10-18 21:55:42 +0200

    Added items in cart indicator above cart icon.

--- frontend/src/components/CartDropdown.jsx
+++ frontend/src/components/CartDropdown.jsx
@@ -10,15 +10,22 @@ export default function CartDropdown() {
     <Dropdown>
       {({ isOpen, close, open, toggle }) => (
         <div onMouseLeave={close} onMouseEnter={open}>
-          <Link
-            to={"/checkout"}
-            onMouseEnter={open}
-            onMouseLeave={close}
-            className="icon block"
-            onClick={close}
-          >
-            <ShoppingCart />
-          </Link>
+          <div className="relative">
+            {cart.length > 0 && (
+              <span className="absolute -top-1 right-0 inline-flex justify-center items-center text-bg-100 bg-dark-200 rounded-full font-medium text-[11px] w-4 h-4 ">
+                {cart.length}
+              </span>
+            )}
+            <Link
+              to={"/checkout"}
+              onMouseEnter={open}
+              onMouseLeave={close}
+              className="icon block"
+              onClick={close}
+            >
+              <ShoppingCart />
+            </Link>
+          </div>
           {isOpen && (
             <div onMouseLeave={close} className="menu-dropdown right-0">
               <ul>
--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,7 +1,16 @@
+import { fetchCart } from "../lib/api.js";
+import { useLoaderData } from "react-router-dom";
+
 export async function loader() {
-  return null;
+  try {
+    const cart = await fetchCart();
+    return { cart };
+  } catch (err) {}
 }
+
 export default function Checkout() {
+  const { cart } = useLoaderData();
+  console.log(cart);
   return (
     <div>
       <h1>Check out</h1>
